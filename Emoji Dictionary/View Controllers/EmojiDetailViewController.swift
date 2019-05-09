62823a5680e31d2e23ac93f10210d3adc76c4f28
Иван Деркачев <longrail@yandex.ru>
2019-05-09 17:51:40 +0300

    Add dismiss keyboard by tap

--- Emoji Dictionary/View Controllers/EmojiDetailViewController.swift
+++ Emoji Dictionary/View Controllers/EmojiDetailViewController.swift
@@ -16,14 +16,24 @@ class EmojiDetailViewController: UIViewController {
     @IBOutlet weak var usageField: UITextField!
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var scrollViewBottomLayout: NSLayoutConstraint!
+    var keyboardDismissTapGesture: UIGestureRecognizer?
     
     var emoji = Emoji()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
-        setupKeyboardNotifications()
         updateUI()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setupKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        removeKeyboardNotifications()
+        super.viewWillDisappear(animated)
     }
     
     func areFieldsReady() -> Bool {
@@ -86,13 +96,39 @@ extension EmojiDetailViewController {
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
+    func removeKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
     @objc func keyboardWillShow(_ notification: NSNotification) {
         if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
             scrollViewBottomLayout.constant = keyboardSize.height
+            addTapRecognizer()
         }
     }
     
     @objc func keyboardWillHide(_ notification: NSNotification) {
        scrollViewBottomLayout.constant = 0
+       removeTapRecognizer()
+    }
+    
+    //dismiss keyboard by tap
+    func addTapRecognizer() {
+        if keyboardDismissTapGesture == nil {
+            keyboardDismissTapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+            keyboardDismissTapGesture?.cancelsTouchesInView = false
+            self.view.addGestureRecognizer(keyboardDismissTapGesture!)
+        }
+    }
+    
+    func removeTapRecognizer() {
+        if keyboardDismissTapGesture != nil {
+            self.view.removeGestureRecognizer(keyboardDismissTapGesture!)
+            keyboardDismissTapGesture = nil
+        }
+    }
+    
+    @objc func dismissKeyboard(sender: UITapGestureRecognizer) {
+        view.endEditing(true)
     }
 }
